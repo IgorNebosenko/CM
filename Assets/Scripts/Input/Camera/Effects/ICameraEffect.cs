7c83c297e17b0fbe2207421275f3571d9626f850
igor_nebosenko <igor.nebosenko@gmail.com>
2022-11-28 02:27:14 +0200

    - Remove old camera system

--- Assets/Scripts/Input/Camera/Effects/ICameraEffect.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CM.Input.CameraController.Effects
-{
-    public interface ICameraEffect
-    {
-        void Execute(float time, float intensity, ICameraController controller);
-        void Simulate(float deltaTime);
-    }
-}
--- Assets/Scripts/Input/Camera/ICameraInput.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace CM.Input.CameraController
-{
-    public interface ICameraInput
-    {
-        void DoRotate(Vector2 rotationAngle);
-    }
-}
